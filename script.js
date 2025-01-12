8d55802929e68eb4456eb4ab3a5093a62b3920f5
nick-anello <anello.nicholas@gmail.com>
2025-01-12 15:12:25 -0600

    disable add button when input is empty

--- script.js
+++ script.js
@@ -2,15 +2,38 @@
  * @fileOverview logic for the bill splitter
  */
 
-//add handler to add a person to the list
-document.getElementById('add-person').addEventListener('click', () => {
-    const peopleList = document.getElementById('people-list');
+// store page elements as variables
+const addButton = document.getElementById('add-person'),
+    personName = document.getElementById('person-name'),
+    peopleList = document.getElementById('people-list');
 
-    //prepend peopleList
+// disable the Add button if input field is blank
+personName.addEventListener('input', () => {
+    addButton.disabled = !personName.value.trim();
+});
+
+// prevent form submission on Enter keypress
+// and add the new person
+personName.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        if (personName.value.trim()) addPerson();
+    }
+});
+
+// add a person to the list
+addButton.addEventListener('click', () => { addPerson(); });
+
+// clear the input field,
+// prepend the people list with the new person,
+// and disable the add button
+function addPerson() {
     peopleList.innerHTML = `
         <li class="list-group-item d-flex justify-content-between">
-            ${document.getElementById('person-name').value.trim()}
+            ${personName.value.trim()}
             <button class="btn btn-sm btn-danger">Remove</button>
         </li>`
         + peopleList.innerHTML;
-});
+    personName.value = '';
+    addButton.disabled = true;
+}
