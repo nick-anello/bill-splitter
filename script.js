0276669fabe4931afac54bc43b827879d36bcf87
nick-anello <anello.nicholas@gmail.com>
2025-01-13 13:37:57 -0600

    handle deletion of a person, and added a maxlength cap to the person name input field

--- script.js
+++ script.js
@@ -2,38 +2,71 @@
  * @fileOverview logic for the bill splitter
  */
 
-// store page elements as variables
+//store page elements as variables
 const addButton = document.getElementById('add-person'),
     personName = document.getElementById('person-name'),
     peopleList = document.getElementById('people-list');
 
-// disable the Add button if input field is blank
+//define globals
+let people = [];
+
+//disable the Add button if input field is blank
 personName.addEventListener('input', () => {
     addButton.disabled = !personName.value.trim();
 });
 
-// prevent form submission on Enter keypress
-// and add the new person
+//handle Enter keypress in the personName input field
 personName.addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
+        //prevent form submission
         event.preventDefault();
+        //if there is input, add the person
         if (personName.value.trim()) addPerson();
     }
 });
 
-// add a person to the list
+//handle the Add People button
 addButton.addEventListener('click', () => { addPerson(); });
 
-// clear the input field,
-// prepend the people list with the new person,
-// and disable the add button
+//handle the Delete buttons in the peopleList
+peopleList.addEventListener('click', (event) => {
+    //check if the clicked element is a Remove button
+    if (event.target.textContent === 'Remove') {
+        //delete the person from the list
+        const li = event.target.closest('li');
+        if (li) li.remove();
+        //update people array
+        updatePeople();        
+    }
+  });
+
+/**
+ * Prepend the people list with the new person
+ */
 function addPerson() {
+    //disable the add button
+    addButton.disabled = true;
+    //prepend the people list
     peopleList.innerHTML = `
         <li class="list-group-item d-flex justify-content-between">
             ${personName.value.trim()}
             <button class="btn btn-sm btn-danger">Remove</button>
         </li>`
         + peopleList.innerHTML;
+    //clear the input field,
     personName.value = '';
-    addButton.disabled = true;
+    //update people array
+    updatePeople();
+}
+
+/**
+ * Update the people array
+ */
+function updatePeople() {
+    //clear people array
+    people.length = 0;
+    //add each person's name to the people array
+    [...peopleList.getElementsByTagName('li')].forEach((li) => {
+        people.push(li.firstChild.textContent.trim());
+    });
 }
