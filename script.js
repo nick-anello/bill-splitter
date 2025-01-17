ec58a4b0a9dbcecb57723ec3c8b3c9a5b7682e90
nick-anello <anello.nicholas@gmail.com>
2025-01-17 12:28:36 -0600

    reordered functions

--- script.js
+++ script.js
@@ -6,8 +6,120 @@
 const state = {
     people: [], // List of people splitting the bill
     items: [], // List of items purchased
-    charges: [] // List of taxes, tips, and/or fees
+    charges: [], // List of taxes, tips, and/or fees
+    moneyInputs: {} // Object whose keys identify which list the input is for, and values are a string of digits
+                    // Example: {items: '1499'}
 };
+
+// Add event listeners to the page container.
+document.getElementById('container').addEventListener('click', handleClick);
+document.getElementById('container').addEventListener('input', handleInput);
+document.querySelectorAll('input[inputmode="decimal"]').forEach(input => input.addEventListener('keydown', handleDecimalTextKeydown));
+
+/**
+ * Handles the click event.
+ * 
+ * @param {Event} event - Click event.
+ * @returns {void}
+ */
+function handleClick(event) {
+    const target = event.target,
+        dataset = target.dataset;
+    switch (dataset.action) {
+        case 'add': addToList(target); break;
+        case 'delete': deleteFromList(dataset.list, dataset.index); break;
+        default:
+    }
+}
+
+/**
+ * Handles the Input event.
+ * Enable the corresponding add button when all related fields contain a value.
+ * 
+ * @param {Event} event - Input event.
+ * @returns {void}
+ */
+function handleInput(event) {
+    const target = event.target,
+        row = target.closest('.row');
+    if (row) {
+        row.querySelector('button').disabled = 
+            [...row.querySelectorAll('input, select')].some(element => !element.value.trim());
+    }
+}
+
+/**
+ * Handles the Input event for text inputs with a decimal inputmode.
+ * Adds digit inputs to the money string, and slices the string on Backspace
+ * 
+ * @param {Event} event - Input event.
+ * @returns {void}
+ */
+function handleDecimalTextKeydown(event) {
+    event.preventDefault();
+    const key = event.key,
+        target = key.target,
+        list = target.dataset.list,
+        inputString = moneyInputs[list];
+    if (/[0-9]/.test(key)) {
+        inputString += key;
+        if (inputString === 0) inputString = '';
+    }
+    else if (key === 'Backspace') {
+        inputString = inputString.slice(0, -1);
+    }
+    renderMoney(target);
+}
+
+/**
+ * Adds values from related input fields to the corresponding list, clears the fields, and updates the UI.
+ * 
+ * @param {HTMLElement} target The Add button that was clicked.
+ * @returns {void}
+ */
+function addToList(target) {
+    target.disabled = true;
+    const list = target.dataset.list,
+        fields = target.closest('.row').querySelectorAll('input, select'),
+        data = [];
+
+    fields.forEach(field => {
+        data.push(field.value.trim());
+        field.value = '';
+    });
+    state[list].unshift(data.length === 1 ? data[0] : data);
+    renderList(list);
+}
+
+/**
+ * Deletes a value from a list and updates the UI.
+ * 
+ * @param {string} list - List to delete from.
+ * @param {number} index - Index to delete.
+ * @returns {void}
+ */
+function deleteFromList(list, index) {
+    state[list].splice(index, 1);
+    renderList(list);
+}
+
+/**
+ * Delegates rendering a list.
+ * 
+ * @param {string} list - List to render.
+ * @returns {void}
+ */
+function renderList(list) {
+    switch (list) {
+        case 'people': 
+        case 'charges':
+            renderSingleFieldList(list);
+            break;
+        case 'items':
+            renderItemsList();
+            break;
+    }
+}
 
 /**
  * Renders a single-field list.
@@ -45,87 +157,11 @@ function renderItemsList() {
 }
 
 /**
- * Delegates rendering a list.
+ * Displays string as USD.
  * 
- * @param {string} list - List to render.
+ * @param {HTMLElement} target - Money input field.
  * @returns {void}
  */
-function renderList(list) {
-    switch (list) {
-        case 'people': 
-        case 'charges':
-            renderSingleFieldList(list);
-            break;
-        case 'items':
-            renderItemsList();
-            break;
-    }
+function renderMoney(target) {
+    
 }
-
-/**
- * Adds values from related input fields to the corresponding list, clears the fields, and renders the list.
- * 
- * @param {HTMLElement} target The Add button that was clicked.
- * @returns {void}
- */
-function addToList(target) {
-    target.disabled = true;
-    const list = target.dataset.list,
-        fields = target.closest('.row').querySelectorAll('input, select'),
-        data = [];
-
-    fields.forEach(field => {
-        data.push(field.value.trim());
-        field.value = '';
-    });
-    state[list].unshift(data.length === 1 ? data[0] : data);
-    renderList(list);
-}
-
-/**
- * Deletes a value from a list and renders it.
- * 
- * @param {string} list - List to delete from.
- * @param {number} index - Index to delete.
- * @returns {void}
- */
-function deleteFromList(list, index) {
-    state[list].splice(index, 1);
-    renderList(list);
-}
-
-/**
- * Handles the click event.
- * 
- * @param {Event} event - Click event.
- * @returns {void}
- */
-function handleClick(event) {
-    const target = event.target,
-        dataset = target.dataset;
-    switch (dataset.action) {
-        case 'add': addToList(target); break;
-        case 'delete': deleteFromList(dataset.list, dataset.index); break;
-        default:
-    }
-}
-
-/**
- * Handles the Input event.
- * Enable the corresponding add button when all related fields contain a value.
- * 
- * @param {Event} event - Input event.
- * @returns {void}
- */
-function handleInput(event) {
-    const target = event.target,
-        row = target.closest('.row');
-    if (row) {
-        row.querySelector('button').disabled = 
-            [...row.querySelectorAll('input, select')].some(element => !element.value.trim());
-    }
-}
-
-// Add event listeners to the page container.
-document.getElementById('container').addEventListener('click', handleClick);
-document.getElementById('container').addEventListener('input', handleInput);
