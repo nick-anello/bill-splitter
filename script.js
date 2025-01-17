ca802a05f6eeabce63fbe4fbec6f4e1b9964662e
nick-anello <anello.nicholas@gmail.com>
2025-01-17 08:08:58 -0600

    implemented add, remove, and handlers, and updated documentation

--- script.js
+++ script.js
@@ -1,60 +1,34 @@
 /**
- * @fileOverview logic for the bill splitter
+ * @fileOverview Logic for the bill splitter app
  */
 
-//store page elements as variables
-
-
-// ==============================
-// Section 2: State
-// ==============================
-// Define the state (data store) for your application
+// State of the bill splitter app
 const state = {
-    people: [],
-    items: [],
-    charges: []
+    people: [], // List of people splitting the bill
+    items: [], // List of items purchased
+    charges: [] // List of taxes, tips, and/or fees
 };
 
-/*
-// ==============================
-// Section 3: rendering
-// ==============================
-// Reusable helper functions to keep your code DRY (Don't Repeat Yourself)
-function buildListElement(list, item, index) {
-    switch (list) {
-        case 'people':
-        case 'charges':
-            return `
-                <li class="list-group-item d-flex justify-content-between">
-                    <span class="text-truncate">${item}</span>
-                    <button class="btn btn-sm btn-danger" data-list="${list}" data-index="${index}">Remove</button>
-                </li>
-            `;
-        case 'items':
-            return `
-                <li class="list-group-item">
-                    <div class="row align-items-center">
-                        <div class="col-3 border-end pe-3 text-truncate">${item.name}</div>
-                        <div class="col-3 border-end pe-3 text-truncate">${item.cost}</div>
-                        <div class="col-3 border-end pe-3 text-truncate">${item.owner}</div>
-                        <div class="col-3 text-end">
-                            <button class="btn btn-sm btn-danger" data-list="${list}" data-index="${index}>Remove</button>
-                        </div>
-                    </div>
-                </li>
-            `;
-        default: return '';
-    }
-}*/
+/**
+ * Renders a single-field list.
+ * 
+ * @param {string} list - List to render.
+ * @returns {void}
+ */
 function renderSingleFieldList(list) {
     document.getElementById(list).innerHTML = state[list].map((value, index) => `
         <li class="list-group-item d-flex justify-content-between">
             <span class="text-truncate">${value}</span>
-            <button class="btn btn-sm btn-danger" data-list="${list}" data-index="${index}">Remove</button>
+            <button class="btn btn-sm btn-danger" data-action="delete" data-list="${list}" data-index="${index}">Remove</button>
         </li>
     `).join('');
 }
 
+/**
+ * Renders the items list.
+ * 
+ * @returns {void}
+ */
 function renderItemsList() {
     document.getElementById('items').innerHTML = state.items.map((item, index) => `
         <li class="list-group-item">
@@ -63,88 +37,94 @@ function renderItemsList() {
                 <div class="col-3 border-end pe-3 text-truncate">${item[1]}</div>
                 <div class="col-3 border-end pe-3 text-truncate">${item[2]}</div>
                 <div class="col-3 text-end">
-                    <button class="btn btn-sm btn-danger data-list="items" data-index="${index}">Remove</button>
+                    <button class="btn btn-sm btn-danger" data-action="delete" data-list="items" data-index="${index}">Remove</button>
                 </div>
             </div>
         </li>
     `).join('');
 }
 
+/**
+ * Delegates rendering a list.
+ * 
+ * @param {string} list - List to render.
+ * @returns {void}
+ */
 function renderList(list) {
-    
     switch (list) {
         case 'people': 
         case 'charges':
             renderSingleFieldList(list);
+            break;
         case 'items':
             renderItemsList();
+            break;
     }
 }
 
-
-
-/*// ==============================
-// Section 4: functions
-// ==============================
-// Functions to update the DOM based on the application state
-function renderList(list) {
-    peopleList.innerHTML = state[list].map((item, index) => buildListElement(list, item, index)).join('');
-}*/
-
+/**
+ * Adds values from related input fields to the corresponding list, clears the fields, and renders the list.
+ * 
+ * @param {HTMLElement} target The Add button that was clicked.
+ * @returns {void}
+ */
 function addToList(target) {
     const list = target.dataset.list,
-        fields = target.closest('.row').querySelectorAll('.form-control, .form-select'),
+        fields = target.closest('.row').querySelectorAll('input, select'),
         data = [];
 
     fields.forEach(field => {
-        data.push(field.value.trim())
+        data.push(field.value.trim());
         field.value = '';
     });
     state[list].unshift(data.length === 1 ? data[0] : data);
     renderList(list);
 }
 
+/**
+ * Deletes a value from a list and renders it.
+ * 
+ * @param {string} list - List to delete from.
+ * @param {number} index - Index to delete.
+ * @returns {void}
+ */
 function deleteFromList(list, index) {
-
+    state[list].splice(index, 1);
+    renderList(list);
 }
 
-/*// ==============================
-// Section 5: Event Handlers
-// ==============================
-// Define functions to handle user interactions
-
-function handlePersonInput(event) {
-    addPersonButton.disabled = !personInput.value.trim();
-}
-
-function handleAddButton() {
-    addPersonButton.disabled = true;
-    state.people.unshift(personInput.value.trim());
-    personInput.value = '';
-    renderList('people');
-}
-function handleListRemoveClick(event) {
-    if (target && target.classList.contains('btn-danger')) {
-        const target = event.target,
-            list = target.getAttribute('data-list');
-        state[list].splice(target.getAttribute('data-index'), 1);
-        renderList(list);
-    }
-}
-*/
+/**
+ * Handles the click event.
+ * 
+ * @param {Event} event - Click event.
+ * @returns {void}
+ */
 function handleClick(event) {
-    const target = event.target;
-    switch (target.dataset.action) {
+    const target = event.target,
+        dataset = target.dataset;
+    switch (dataset.action) {
         case 'add': addToList(target); break;
-        case 'delete': deleteFromList(list, dataset.index); break;
+        case 'delete': deleteFromList(dataset.list, dataset.index); break;
         default:
     }
 }
 
-// ==============================
-// Section 6: Initialization
-// ==============================
-// Set up event listeners and perform any setup tasks
-document.getElementById('container').addEventListener('click', handleClick);
+/**
+ * Handles the Input event.
+ * Enable the corresponding add button when all related fields contain a value.
+ * 
+ * @param {Event} event - Input event.
+ * @returns {void}
+ */
+function handleInput(event) {
+    const target = event.target,
+        row = target.closest('.row');
+    if (row) {
+        row.querySelector('button').disabled = 
+            [...row.querySelectorAll('input, select')].some(element => !element.value.trim());
+    }
+}
 
-// Call the init function to start the app
+// Add event listeners to the page container.
+document.getElementById('container').addEventListener('click', handleClick);
+document.getElementById('container').addEventListener('input', handleInput);
